620e64eb3e533df742f0f720a5fb6f8c3ac21fbb
sogatanco <adeliapucha46@gmail.com>
2019-04-10 23:42:40 +0700

    added NoActionBar and FullScreen style

--- app/src/main/java/site/team2dev/nemubengkel/HomeFragment.java
+++ app/src/main/java/site/team2dev/nemubengkel/HomeFragment.java
@@ -8,48 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.MapView;
-import com.google.android.gms.maps.MapsInitializer;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.model.MarkerOptions;
-
-public class HomeFragment extends Fragment implements OnMapReadyCallback {
-
-    GoogleMap mgoogleMap;
-    MapView mapView;
-
-
-    View view;
+public class HomeFragment extends Fragment {
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        view= inflater.inflate(R.layout.home_fragment, null);
-        return view;
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        super.onViewCreated(view, savedInstanceState);
-
-        mapView= mapView.findViewById(R.id.map);
-        if(mapView!=null){
-            mapView.onCreate(null);
-            mapView.onResume();
-            mapView.getMapAsync(this);
-        }
-    }
-
-
-    @Override
-    public void onMapReady(GoogleMap googleMap) {
-        MapsInitializer.initialize(getContext());
-
-        mgoogleMap=googleMap;
-        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-
-        googleMap.addMarker(new MarkerOptions().position(neż))
+        return inflater.inflate(R.layout.mobil_fragment, null);
 
     }
 }
