84cc91e3c0d13c65351640d58071a73d2998d0b7
sogatanco <adeliapucha46@gmail.com>
2019-04-10 16:32:42 +0700

    added fragment activity

--- app/src/main/java/site/team2dev/nemubengkel/MainActivity.java
+++ app/src/main/java/site/team2dev/nemubengkel/MainActivity.java
@@ -3,45 +3,61 @@ package site.team2dev.nemubengkel;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private TextView mTextMessage;
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText("Home");
-                    return true;
-                case R.id.navigation_mobil:
-                    mTextMessage.setText("Mobil");
-                    return true;
-                case R.id.navigation_motor:
-                    mTextMessage.setText("Motor");
-                    return true;
-                case R.id.navigation_profil:
-                    mTextMessage.setText("Motor");
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+
+        loadFragment(new HomeFragment());
     }
 
+    private boolean loadFragment(Fragment fragment){
+        if(fragment != null){
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+
+            return  true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+        Fragment fragment=null;
+        switch (menuItem.getItemId()){
+            case R.id.navigation_home:
+                fragment=new HomeFragment();
+                break;
+
+            case R.id.navigation_mobil:
+                fragment=new MobilFragment();
+                break;
+
+            case R.id.navigation_motor:
+                fragment=new MotorFragment();
+                break;
+
+            case R.id.navigation_profil:
+                fragment=new ProfilFragment();
+                break;
+
+        }
+        return loadFragment(fragment);
+    }
 }
